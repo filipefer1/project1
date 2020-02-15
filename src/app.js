05ed6254491d715d8f289006b6516a6aa359fcca
Adm <filipefernandes000@gmail.com>
2020-02-15 14:35:16 -0300

    Inicializar as variáveis ambientes

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+require('dotenv/config');
+
 const express = require('express');
 const mongoose = require('mongoose')
 
@@ -8,7 +10,7 @@ app.use(express.json());
 
 app.use(routes);
 
-mongoose.connect(`mongodb+srv://filipe:255287@noqslmodule-kf7s8.mongodb.net/project1?retryWrites=true&w=majority`, {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
